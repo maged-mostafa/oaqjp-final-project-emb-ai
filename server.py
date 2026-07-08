738623ded0bb33d0a673d4157830d5dea65afd77
maged-mostafa <maged.mostafa@gmx.de>
2026-07-08 22:02:32 +0200

    update

--- server.py
+++ server.py
@@ -14,12 +14,12 @@ def render_index_page():
 
 @app.route("/emotionDetector")
 def sent_analyzer():
-    """
-    Analyze the submitted text and return the detected emotions.
-    """
     text_to_analyze = request.args.get("textToAnalyze")
 
     response = emotion_detector(text_to_analyze)
+
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
 
     return (
         f"For the given statement, the system response is "
@@ -28,8 +28,7 @@ def sent_analyzer():
         f"'fear': {response['fear']}, "
         f"'joy': {response['joy']}, "
         f"'sadness': {response['sadness']}. "
-        f"The dominant emotion is "
-        f"{response['dominant_emotion']}."
+        f"The dominant emotion is {response['dominant_emotion']}."
     )
 
 
